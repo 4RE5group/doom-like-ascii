f4a16a18c9bcc7ee1b9f3feaae4df2ad3d6a2ef3
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2025-05-20 11:47:35 +0200

    Update main.c

--- src/C/main.c
+++ src/C/main.c
@@ -3,6 +3,8 @@
 #include <math.h>
 
 
+#define gotoxy(x,y) printf("\033[%d;%dH", (y), (x))
+
 #define SCREEN_HEIGHT 20
 #define SCREEN_WIDTH 53
 #define MAP_SIZE 10
@@ -18,6 +20,10 @@ float playerY = 5.0f;
 float playerAngle = 0.0f;
 
 
+// buffers 
+char** screen;
+char** map;
+
 void trace_line(int l, int h, char c, char*** screen) {
     if (*screen == NULL) return;
     int padding = (int)((SCREEN_HEIGHT - h) / 2);
@@ -31,21 +37,46 @@ void trace_line(int l, int h, char c, char*** screen) {
 }
 
 int distance(float angle) {
-    
+    float rayDirX = (float)cos(angle);
+    float rayDirY = (float)sin(angle);
+
+    float rayPosX = playerX;
+    float rayPosY = playerY;
+
+    float distance = 0.0f;
+
+    while(1) {
+        if(rayPosX < 0 || rayPosY < 0 || rayPosX > sizeof(map) || rayPosY > sizeof(map))
+            return -1;
+
+        if(distance > RENDER_DIST)
+            return -1;
+
+        distance+=rayStep;
+        rayPosX+=rayDirX*rayStep;
+        rayPosY+=rayDirY*rayStep;
+
+        if(map[(int)rayPosY][(int)rayPosX] == '#')
+            return distance;
+    }
 }
 
 int main(int argc, char* argv[]) {
     // Alloc mem for screen buffer
-    char** screen = malloc(SCREEN_HEIGHT * sizeof(char*));
-    if (screen == NULL) {
+    screen = malloc(SCREEN_HEIGHT * sizeof(char*));
+    map = malloc(MAP_SIZE * sizeof(char*));
+
+
+    if (screen == NULL || map == NULL) {
         fprintf(stderr, "Memory allocation failed\n");
         return 1;
     }
+    // fill up the screen buffer
     for (int y = 0; y < SCREEN_HEIGHT; y++) {
         screen[y] = malloc(SCREEN_WIDTH * sizeof(char));
         if (screen[y] == NULL) {
             fprintf(stderr, "Memory allocation failed\n");
-            // Free previously allocated memory
+            // free previously allocated memory
             for (int i = 0; i < y; i++) {
                 free(screen[i]);
             }
@@ -56,21 +87,31 @@ int main(int argc, char* argv[]) {
             screen[y][x] = ' ';
         }
     }
+    // now time for the map initialisation
+    for(int y=0; y<MAP_SIZE; y++) {
+        map[y] = malloc(MAP_SIZE * sizeof(char*));
+        if(map[y] == NULL) {
+            sprintf(stderr, "Memory allocation failed\n");
+            for (int y = 0; y < MAP_SIZE; y++) {
+                free(map[y]);
+            }
+            free(map);
+            return 1;
+        }
+        for (int x = 0; x < MAP_SIZE; x++) {
+            map[y][x] = ' ';
+        }
+    }
 
     // Clear screen
     printf("\e[1;1H\e[2J");
-    float degPerColumn = (2*PI)/SCREEN_WIDTH;
-    float angle=0;
-    for(int c=0; c<SCREEN_WIDTH; c++) {
-       angle += degPerColumn;
-       // y=ax+b
-       for(int x=0; x<RENDER_DIST; x++) {
-           // round(x,y) -> player pos in matrix
-           // 
-       }
-       printf("%fx\n", cos(angle));
-       trace_line(c, 4, '#', &screen);
+    
+    int key;
+    while(1) {
+        key = getchar();
+        printf("%d\n", key);
     }
+
 
     // Render
     for (int y = 0; y < SCREEN_HEIGHT; y++) {
@@ -80,12 +121,20 @@ int main(int argc, char* argv[]) {
         printf("\n"); // Add newline for each row
     }
 
-    // Free allocated memory
+
+
+
+
+    // free allocated memory
     for (int y = 0; y < SCREEN_HEIGHT; y++) {
         free(screen[y]);
     }
     free(screen);
+    // now map
+    for (int y = 0; y < MAP_SIZE; y++) {
+        free(map[y]);
+    }
+    free(map);
 
     return 0;
 }
-
